d530dd55b457b81e1e629c224b95bf915333c635
mzgui <gmzinf@gmail.com>
2025-09-10 08:56:04 -0300

    Add initial C source files for coding exercises
    - Created coding_time.c and desafio.c as placeholders for future implementations.
    - Implemented estrutura_decisao.c with various conditional statements demonstrating comparisons, age checks, temperature evaluation, grading, and stock management.

---
+++ Tema_3/Novato/estrutura_decisao.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+  int numero1 = 8;
+  int numero2 = 5;
+
+  if (numero1 > numero2) {
+    printf("numero1 é maior que numero2\n");
+  }
+
+  int idade = 20;
+
+  if (idade >= 18) {
+    printf("Você é maior de idade\n");
+  }
+
+  float temperatura = 32.0;
+
+  if (temperatura > 30.0) {
+    printf("Está calor\n");
+  }
+
+    int nota = 75;
+
+  if (nota >= 60) {
+    printf("Você passou!\n");
+  }
+    int idade1 = 25;
+  int idade2 = 30;
+
+  if (idade1 > idade2) {
+    printf("Pessoa 1 é mais velha que Pessoa 2\n");
+  }
+
+    int numero = 4;
+
+  if (numero % 2 == 0) {
+    printf("O número é par\n");
+  }
+
+  int estoque = 3;
+
+  if (estoque < 5) {
+    printf("Estoque baixo\n");
+  }
+  
+  return 0;
+
+  
+}
